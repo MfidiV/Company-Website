63850469782f57a960f2d00af08e66d986062349
Bunono <mfidiv@gmail.com>
2024-04-03 19:04:00 +0200

    footer backgroung color and margins

--- Isringhausen/src/Components/Footer/Footer.jsx
+++ Isringhausen/src/Components/Footer/Footer.jsx
@@ -92,12 +92,12 @@ function Footer() {
         </div>
       </section>
       <div
-        className="text-center p-4"
+        className=" m-2 text-center p-4"
         style={{ backgroundColor: "rgba(0, 0, 0, 0.05)" }}
       >
-        © 2021 Copyright:
+        © 2024 Copyright:
         <a className="text-reset fw-bold" href="https://mdbootstrap.com/">
-          MDBootstrap.com
+          Mfidi Vuyolwethu
         </a>
       </div>
     </footer>
