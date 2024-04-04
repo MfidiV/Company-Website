8f08914cc91021f24757cb5e5b75ba9eb4164dff
Bunono <mfidiv@gmail.com>
2024-04-04 21:15:14 +0200

    Applied rsponsiveness on navbar and functionality oon smaller screens.

--- Isringhausen/src/Components/Header/Header.jsx
+++ Isringhausen/src/Components/Header/Header.jsx
@@ -1,28 +1,36 @@
-import React from 'react';
-import Logo from '../../assets/Logo/isri.gif'
-import './Header.css'
+import React, { useState } from 'react';
+import Logo from '../../assets/Logo/isri.gif';
+import './Header.css';
+
 function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
+
   return (
     <header id="header" className="header d-flex align-items-center bg-light sticky-top">
       <div className="container-fluid container-xl d-flex align-items-center justify-content-between flex-row">
         <a href="index.html" className="logo d-flex align-items-center text-decoration-none">
-          <img src={Logo}alt="Logo" />
+          <img src={Logo} alt="Logo" />
         </a>
-        <nav id="navbar" className="navbar">
-          <ul className="navbar-nav d-flex flex-row">
+        <nav id="navbar" className={`navbar ${isMenuOpen ? 'open' : ''}`}>
+          <ul className="navbar-nav">
             <li className="nav-item"><a className="nav-link px-3" href="#hero">Home</a></li>
             <li className="nav-item"><a className="nav-link px-3" href="#about">About</a></li>
             <li className="nav-item"><a className="nav-link px-3" href="#services">Services</a></li>
             <li className="nav-item"><a className="nav-link px-3" href="#events">Events</a></li>
             <li className="nav-item"><a className="nav-link px-3" href="#team">Team</a></li>
+            <li className="nav-item"><a className="nav-link px-3" href="#contact">Contact</a></li>
             <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a className="nav-link dropdown-toggle px-3" href="#" id="navbarDropdown" role="button" >
                 Downloads
               </a>
               <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
                 <li><a className="dropdown-item" href="#">General terms and Conditions</a></li>
                 <li className="dropdown">
-                  <a className="dropdown-item dropdown-toggle" href="#" id="navbarDropdown2" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <a className="dropdown-item dropdown-toggle" href="#" id="navbarDropdown2" role="button">
                     Deep Drop Down
                   </a>
                   <ul className="dropdown-menu" aria-labelledby="navbarDropdown2">
@@ -38,18 +46,16 @@ function Header() {
                 <li><a className="dropdown-item" href="#">Drop Down 4</a></li>
               </ul>
             </li>
-            <li className="nav-item"><a className="nav-link px-3" href="#contact">Contact</a></li>
+           
           </ul>
         </nav>
         {/* .navbar */}
        {/* Toggle button for mobile navigation */}
-       <button className="mobile-nav-toggle btn btn-outline-dark d-block d-md-none" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          &#9776; {/* Hamburger icon */}
-          &#10006; {/* Close icon */}
+       <button className="mobile-nav-toggle btn btn-outline-dark d-block d-md-none" type="button" onClick={toggleMenu}>
+          {isMenuOpen ? <span>&#10006;</span> : <span>&#9776;</span>}
         </button>
       </div>
     </header>
-    
   );
 }
 
