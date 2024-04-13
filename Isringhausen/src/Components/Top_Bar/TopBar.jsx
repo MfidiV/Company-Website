fc331abf8f4629850092092cb0502f3fed259afe
Bunono <mfidiv@gmail.com>
2024-04-13 15:50:54 +0200

    topBar changed styles to accomodate responsive

--- Isringhausen/src/Components/Top_Bar/TopBar.jsx
+++ Isringhausen/src/Components/Top_Bar/TopBar.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import './TopBar.css'; // Import custom CSS file for styling
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faPhone } from '@fortawesome/free-solid-svg-icons';
@@ -7,14 +8,17 @@ import { faTwitter, faFacebook, faInstagram, faLinkedin } from '@fortawesome/fre
 const TopBar = () => {
   return (
     <section id="topbar" className="topbar py-2 bg-dark text-light">
-      <div className="container">
-        <div className="row justify-content-between align-items-center">
+      <div className=" container">
+        <div className="top-b row justify-content-around align-items-center">
           <div className="col-md-6 col-lg-8 contact-info">
             <FontAwesomeIcon icon={faEnvelope} className="me-2" />
             <a href="mailto:contact@example.com" className="text-decoration-none text-light">reception@bunoseats.co.za</a>
-            <span className="ms-2 me-2">|</span>
+            {/* <span className="ms-2 me-2">|</span> */}
+            <div>
             <FontAwesomeIcon icon={faPhone} className="me-2" />
             <span>043 234 5678</span>
+            </div>
+            
           </div>
           <div className="col-md-6 col-lg-4 social-links text-md-end mt-2 mt-md-0">
             <a href="#" className="twitter text-light me-3"><FontAwesomeIcon icon={faTwitter} /></a>
