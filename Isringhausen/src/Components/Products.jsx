7a4aad595147a602cd5ac4585453115cb2d0a9f4
Vuyolwethu_Mfidi <mfidiv@gmail.com>
2024-07-22 10:59:41 +0200

    Updates on assets and overall content to fit clients changes

--- Isringhausen/src/Components/Products.jsx
+++ Isringhausen/src/Components/Products.jsx
@@ -1,97 +1,92 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+// import Testimonials from "../Components/Testimonials"
 import "../Styles/products.css";
-import image01 from "../assets/Seat_Products/2021-mine-3000.png";
-import image02 from "../assets/Seat_Products/img01.jpg";
-import image03 from "../assets/Seat_Products/img02.jpg";
-import image04 from "../assets/Seat_Products/img04.jpg";
-import image05 from "../assets/Seat_Products/img12.jpg";
-import image06 from "../assets/Seat_Products/ISRI-6830KM-870-AIR-SEAT-500x749.jpg";
-import image07 from "../assets/Seat_Products/6030_880_Large.png";
-import image08 from "../assets/Seat_Products/Bus.jpg";
-import image09 from "../assets/Seat_Products/img11.jpg";
-import image10 from "../assets/Seat_Products/Bus06.jpg";
-import image11 from "../assets/Seat_Products/VOLVO-air-seat-shovel-ISRI-air-6500-500x678.jpg";
-import image12 from "../assets/Seat_Products/csm_tabbar_lkw_418d154521.jpg";
+import image01 from "../assets/Seat_Products/office1.png";
+import image02 from "../assets/Seat_Products/hom4.png";
+import image03 from "../assets/Seat_Products/Office2.jpg";
+import image04 from "../assets/Seat_Products/hom1.png";
+import image05 from "../assets/Seat_Products/outdoor2.png";
+import image06 from "../assets/Seat_Products/outdoor1.png";
+import image07 from "../assets/Seat_Products/hom3.png";
+import image08 from "../assets/Seat_Products/hom2.png";
+import image09 from "../assets/Seat_Products/hom5.png";
+import image10 from "../assets/Seat_Products/Office3.png";
+import image11 from "../assets/Seat_Products/office4.png";
+import image12 from "../assets/Seat_Products/foldingTable.png";
+
 
 const images = [
   {
     src: image08,
-    alt: "Bus.png",
-    desc: "Commuter Seat 2005",
-    applications: ["Bus", "Public Transport"],
-    features: ["Ergonomic Design", "Durable Material"],
-    options: ["Custom Upholstery", "Adjustable Headrest"],
+    alt: "Finger Shaped.png",
+    'desc': "Finger Shaped 005",
+   " applications": ["Perfect for living rooms, reading nooks, and lounges"],
+    "features": ["Ergonomic Design", "Durable Material"],
+    "options": ["Custom Upholstery", "Adjustable Headrest"],
   },
   {
     src: image02,
-    alt: "Commuter 2000.jpg",
-    desc: "Commuter 2000 Bus Seat",
-    applications: ["Bus", "Shuttle"],
-    features: ["High Durability", "Comfort Padding"],
-    options: ["Various Colors", "Armrests"],
+    alt: "Strain Net.jpg",
+    "desc": "Strain Net 001",
+    "applications": ["Ideal for dining rooms and kitchen seating"],
+    "features": ["High Comfort", "Ergonomic Design"],
+    "options": ["Various Fabrics", "Reclining Function"]
   },
   {
     src: image09,
-    alt: "img11.jpg",
-    desc: "Commuter",
-    applications: ["Commuter Vehicles"],
-    features: ["Reclining Mechanism", "Adjustable Height"],
-    options: ["Leather Upholstery", "Integrated Seatbelt"],
+    alt: "Elegance Dining chair.jpg",
+    desc: "Elegance Dining Chair",
+    applications: ["Suited for living rooms, home theaters, and relaxation spaces"],
+    features: [" Upholstered seat and backrest with high-density foam", "Solid wood legs with a walnut finish","Ergonomic design for comfort during long meals","Available in multiple colors and fabrics"],
+    options: ["Single Chair", "Set of 2,4,6"],
   },
   {
     src: image04,
-    alt: "img04.jpg",
-    desc: "Bus commuter Seat",
-    applications: ["Bus"],
+    alt: "Traditional pieces.jpg",
+    desc: "Traditional pieces",
+    applications: ["Suited for dinning area"],
     features: ["Anti-Slip Material", "Lightweight"],
     options: ["Cup Holder", "Reading Light"],
   },
   {
     src: image05,
-    alt: "img12.jpg",
-    desc: "ISRI 6860/870 AIR-SEAT",
-    applications: ["Trucks", "Construction Vehicles"],
-    features: ["Air Suspension", "Adjustable Armrests"],
-    options: ["Heated Seat", "Massage Function"],
+    alt: "Patio Paradise.jpg",
+    desc: "Patio Paradise",
+    applications: ["Ideal for patios, balconies, and outdoor dining areas"],
+    features: ["Weather-resistant", "Lightweight and stackable design"],
+    options: ["Colors","Single chair","Set of 4", "Massage Function"],
   },
   {
     src: image06,
-    alt: "ISRI-6830KM-870-AIR-SEAT-500x749.jpg",
-    desc: "ISRI 6830KM/880 AIR SEAT",
-    applications: ["Truck Driver Seat"],
+    alt: "Garden Oasis Bench.jpg",
+    desc: "Garden Oasis Bench",
+    applications: ["Perfect for garden settings, parks, and backyard seating"],
     features: [
-      "Backrest Reclining Mechanism",
-      "Seat Height & Slope Adjustment",
-      "Suspension Dust Cover",
+      "Solid teak wood construction",
+      "Durable and weather-resistant finish",
+      "Easy assembly with reliable hardware",
       "Front Operated Double Locking Slides"
     ],
     options: [
-      "Style of Backrest – 575, 577",
-      "2 Point Safety Lap Belt",
-      "RH or LH Operating Handles",
-      "Adjustable Armrests",
-      "Headrest",
-      "Cloth or Vinyl Upholstery (Local & Imported)",
-      "ABS Backing",
-      "Turntable",
-      "Side operated Reinforced slides"
+      "Bench fold",
+      "Removable Seats"
     ],
   },
   {
     src: image07,
-    alt: "6030_880_Large.png",
-    desc: "ISRI 6030/880 Large",
-    applications: ["Heavy Duty Trucks"],
+    alt: "Poolside Comfort Lounger.png",
+    desc: "Poolside Lounger",
+    applications: ["Ideal for patios and outdoor dining areas"],
     features: ["Heavy Duty Suspension", "Wide Seat Base"],
     options: ["Gel Cushion", "Custom Embroidery"],
   },
   {
     src: image12,
-    alt: "Isri_NTS2.jpg",
-    desc: "NTS-2 6860kM/870",
-    applications: ["Commercial Vehicles"],
-    features: ["Compact Design", "Energy Absorption"],
+    alt: "Stainless bench.jpg",
+    desc: "Stainless steel bench",
+    applications: ["Perfect for garden settings, parks, and backyard seating"],
+    features: ["Easy assembly with stainless steel hardware", "Energy Absorption"],
     options: ["Cooling System", "Integrated Speakers"],
   },
   {
@@ -104,26 +99,26 @@ const images = [
   },
   {
     src: image03,
-    alt: "img02.jpg",
-    desc: "ISRI 100/575 driver Seat",
-    applications: ["Trucks"],
-    features: ["Basic Suspension", "Ergonomic Shape"],
+    alt: "ErgoPro.jpg",
+    desc: "ErgoPro Office Chair",
+    applications: ["Ideal for office workstations and home offices"],
+    features: ["Adjustable lumbar support", "Breathable mesh backrest","360-degree swivel and smooth-rolling casters","","Fixed armrests"],
     options: ["Adjustable Lumbar", "Vibration Dampening"],
   },
   {
     src: image10,
-    alt: "Bus06",
-    desc: "ISRI 6000/577 driver Seat",
-    applications: ["Buses"],
-    features: ["High Backrest", "Adjustable Armrests"],
-    options: ["Integrated Controls", "Memory Foam"],
+    alt: "Executive Comfort Chair.png",
+    desc: "Executive Comfort Chair",
+    applications: ["Perfect for executive offices and managerial seating"],
+    features: ["High backrest with built-in headrest", "Premium bonded leather upholstery"],
+    options: ["Waterproof Cover", "Multi-function tilt mechanism"],
   },
   {
     src: image01,
-    alt: "2021-mine-300.jpg",
-    desc: "2021-mine-300",
-    applications: ["Mining Vehicles"],
-    features: ["Heavy Duty Build", "Enhanced Safety"],
+    alt: "Conference Elite Chair.jpg",
+    desc: "Conference Elite Chair",
+    applications: ["Suited for conference rooms and meeting spaces"],
+    features: ["Padded seat for extended comfort", "360-degree swivel with smooth-rolling casters","Mid-back mesh design for optimal airflow"],
     options: ["Adjustable Headrest", "Removable Cushion"],
   },
 ];
@@ -131,27 +126,31 @@ const images = [
 const Products = () => {
   const navigate = useNavigate();
 
-
   return (
-    <div className="products" id="products">
-      <div className="heading">
-        <h2>Products</h2>
-      </div>
-      <div className="gallery-container">
-        {images.map((image, index) => (
-          <div className="responsive" key={index}>
-            <div className="gallery">
-              <img
-                src={image.src}
-                alt={image.alt}
-                onClick={() => navigate("/SelectedImage", { state: { image } })}
-              />
-              <div className="desc">{image.desc}</div>
+    <>
+      <div className="products" id="products">
+        <div className="heading">
+          <h2>Products</h2>
+        </div>
+        <div className="gallery-container">
+          {images.map((image, index) => (
+            <div className="responsive" key={index}>
+              <div className="gallery">
+                <img
+                  src={image.src}
+                  alt={image.alt}
+                  onClick={() =>
+                    navigate("/SelectedImage", { state: { image } })
+                  }
+                />
+                <div className="desc">{image.desc}</div>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
-    </div>
+      {/* <Testimonials /> */}
+    </>
   );
 };
 
